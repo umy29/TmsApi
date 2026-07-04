d8601e8eb286d68497b0e7c3196dd212f4478efc
oumer wassu <omar2964eth@gmail.com>
2026-07-04 15:25:07 +0300

    M5S2 Ex4: per-entity IEntityTypeConfiguration classes, RefineTmsModel migration

---
+++ Data/Configurations/EnrollmentConfiguration.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TmsApi.Entities;
+
+namespace TmsApi.Data.Configurations;
+
+public class EnrollmentConfiguration : IEntityTypeConfiguration<Enrollment>
+{
+    public void Configure(EntityTypeBuilder<Enrollment> builder)
+    {
+        builder.HasKey(e => e.Id);
+
+        builder.HasOne(e => e.Student)
+            .WithMany(s => s.Enrollments)
+            .HasForeignKey(e => e.StudentId);
+
+        builder.HasOne(e => e.Course)
+            .WithMany(c => c.Enrollments)
+            .HasForeignKey(e => e.CourseId);
+    }
+}
---
+++ Data/Configurations/StudentConfiguration.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TmsApi.Entities;
+
+namespace TmsApi.Data.Configurations;
+
+public class StudentConfiguration : IEntityTypeConfiguration<Student>
+{
+    public void Configure(EntityTypeBuilder<Student> builder)
+    {
+        builder.HasKey(s => s.Id);
+
+        builder.Property(s => s.RegistrationNumber)
+            .IsRequired()
+            .HasMaxLength(20);
+
+        builder.Property(s => s.Name)
+            .IsRequired()
+            .HasMaxLength(200);
+    }
+}
