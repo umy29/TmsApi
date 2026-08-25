1eb660998cac02b712c43e3e0a9596993bea3d08
oumer wassu <omar2964eth@gmail.com>
2026-08-25 11:14:59 +0300

    feat(module-11/session-3): add CourseInstructorRequirement (Ex5 Step 1)

---
+++ TmsApi.Api/Authorization/CourseInstructorRequirement.cs
@@ -0,0 +1,6 @@
+using Microsoft.AspNetCore.Authorization;
+
+namespace TmsApi.Api.Authorization;
+
+// Module 11 - Session 3 - Exercise 5: resource-based authorization requirement
+public class CourseInstructorRequirement : IAuthorizationRequirement { }
